b63daa2dc80b9082c74df2d22a204dfae825e956
Xia Wei <e0022672@nus.edu.sg>
2019-01-06 17:22:06 +0800

    Update the setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opdelft",
+    version="0.0.1",
+    author="xia wei",
+    author_email="xiawei@u.nus.edu",
+    description="An optimization toolbox for expensive environmental models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/louisXW/opdelft",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy", "pySOT==0.1.36", "poap-0.1.26"],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
