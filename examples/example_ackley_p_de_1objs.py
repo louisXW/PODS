751f2d03c2d239643914da6bb27c9dc05a56fe8b
Xia Wei <e0022672@nus.edu.sg>
2019-01-06 19:59:16 +0800

    Update the example_ackley_p_de_10bjs.py

--- examples/example_ackley_p_de_1objs.py
+++ examples/example_ackley_p_de_1objs.py
@@ -1,6 +1,6 @@
-from op_delf.algorithms.differential_evoluation._differentialevolution import *
+from opdelft.algorithms.differential_evoluation._differentialevolution import *
 # from ObjectlongbothPDE import *
-from op_delf.problems.test_functions import *
+from opdelft.problems.test_functions import *
 import os.path
 import logging
 import os
